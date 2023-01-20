28c48945d560b6f115ef0020639f8ef29fd9ea8f
hyoloui <tmdgy9272@gmail.com>
2023-01-20 17:42:05 +0900

    feat: main layout
    Main / Youtube 레이아웃 초안 완성
    
    Related to: #2
    by @hyoloui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Main from './pages/Main';
 function App() {
   return (
     <div className="App">
-      <h1>develife 메인페이지</h1>
       <Main />
     </div>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,19 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import YoutubeBoard from '../components/YoutubeBoard';
 
 function Main() {
-  return <h1>Main 페이지</h1>;
+  return (
+    <MainWrapper>
+      <YoutubeBoard></YoutubeBoard>
+    </MainWrapper>
+  );
 }
 
 export default Main;
+
+const MainWrapper = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  border: 1px solid red;
+`;
