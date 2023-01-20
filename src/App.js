d409796362b6476132071919145e4c63957f471e
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-01-20 17:51:52 +0900

    feat: create Navbar layout 레이아웃 초안 완성 Related to: #2 by @hihee

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Navbar from './components/Navbar';
+import Category from './pages/Category';
 import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>develife 메인페이지</h1>
+      <Navbar />
       <Main />
+      <Category />
     </div>
   );
 }
